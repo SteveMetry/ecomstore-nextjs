e359a2ed9cd886250bbf7e16d56436619d1d6a66
Steven <steven.metry@icloud.com>
2023-01-05 14:55:47 +1300

    added auth0 login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import { NEXT_SEO_DEFAULT } from "next-seo.config";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 import ".styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <UserProvider>
       <DefaultSeo {...NEXT_SEO_DEFAULT} />
       <Component {...pageProps} />
-    </>
+    </UserProvider>
   );
 }
